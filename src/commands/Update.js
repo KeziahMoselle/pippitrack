009a15db1951a535880324b4b8dfa6c4635e4c50
Keziah MOSELLE <apps@keziahmoselle.fr>
2021-07-12 17:36:59 +0200

    feat: improve update command

--- src/commands/Update.js
+++ src/commands/Update.js
@@ -23,19 +23,56 @@ class UpdateCommand {
     try {
       const { data: difference } = await axios.post(this.UPDATE_ENDPOINT(user.id))
 
+      // This player hasn't been tracked
+      if (difference.first) {
+        const embed = new MessageEmbed()
+          .setTitle(`${user.name} is now tracked on osu!track`)
+          .setDescription('Play a bit and update again to see difference in stats since the last update.')
+          .setURL(`https://ameobea.me/osutrack/user/${encodeURIComponent(user.name)}`)
+
+        return message.channel.send(embed)
+      }
+
+      const pp_rank = difference.pp_rank > 0 ? `-${difference.pp_rank}` : `${difference.pp_rank}`.replace('-', '+')
+      let pp_rank_diff
+
+      // The player is losing ranks
+      if (difference.pp_rank > 0) {
+        pp_rank_diff = Number(user.pp.rank) + Number(difference.pp_rank)
+      }
+
+      // The player is gaining ranks
+      if (difference.pp_rank < 0) {
+        pp_rank_diff = Number(user.pp.rank) - Number(`${difference.pp_rank}`.replace('-', ''))
+      }
+
+      const pp_raw = Number.parseFloat(difference.pp_raw).toPrecision(4)
+      const accuracy = Number.parseFloat(difference.accuracy).toPrecision(4)
+
       const embed = new MessageEmbed()
         .setTitle(`Changes since last update for ${user.name}`)
         .setThumbnail(`http://s.ppy.sh/a/${user.id}`)
-        .addField('Playcount', `+${difference.playcount}`, true)
-        .addField('Rank', `${difference.pp_rank}`, true)
-        .addField('PP', `${difference.pp_raw}`, true)
-        .addField('Accuracy', `${difference.accuracy}`, true)
         .setURL(`https://ameobea.me/osutrack/user/${encodeURIComponent(user.name)}`)
         .setColor(11279474)
+        .addField('Rank gained', `${pp_rank}`, true)
+        .addField('Playcount', `+${difference.playcount}`, true)
+
+      if (pp_rank_diff) {
+        embed.addField('Previous Rank', pp_rank_diff, true)
+        embed.addField('Current rank', user.pp.rank, true)
+      }
+
+      if (pp_raw >= 1) {
+        embed.addField('PP', `${pp_raw}`, true)
+      }
+
+      if (accuracy >= 0.01) {
+        embed.addField('Accuracy', `${accuracy}`, true)
+      }
 
       if (difference.newhs.length > 0) {
         const newHighscores = difference.newhs.reduce((list, highscore) => {
-          return list + `**+${highscore.pp}** - ${highscore.rank} (#${highscore.ranking})\n`
+          return list + `:${highscore.rank}: **+${Math.round(highscore.pp)}pp** (Personal best #${highscore.ranking + 1})\n`
         }, 'New highscores :\n')
 
         embed.setDescription(newHighscores)
