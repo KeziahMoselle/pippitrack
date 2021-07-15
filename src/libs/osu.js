7186816231dee2f3ef5748889a2d326042d3a86a
Nemesis <keziah.moselle@gmail.com>
2021-07-15 22:57:26 +0200

    feat: osu api v2 class (#1)
    closes #1

--- src/libs/osu.js
+++ src/libs/osu.js
@@ -1,3 +1,4 @@
+const axios = require('axios').default
 const osu = require('node-osu')
 
 const osuApi = new osu.Api(process.env.OSU_API_KEY, {
@@ -6,6 +7,54 @@ const osuApi = new osu.Api(process.env.OSU_API_KEY, {
   notFoundAsError: false
 })
 
+class OsuApiv2 {
+  name = 'Bancho'
+  base = 'https://osu.ppy.sh/api/v2/'
+  userUrlBase = 'https://osu.ppy.sh/users/'
+  avatarUrl = 'http://s.ppy.sh/a/'
+  clientId = null
+  clientSecret = null
+  token = null
+  tokenExpire = null
+
+  constructor (clientId, clientSecret) {
+    this.clientId = clientId
+    this.clientSecret = clientSecret
+  }
+
+  async getToken () {
+    const { data } = await axios.post('https://osu.ppy.sh/oauth/token', {
+      client_id: this.clientId,
+      client_secret: this.clientSecret,
+      grant_type: 'client_credentials',
+      scope: 'public'
+    })
+
+    this.token = data.access_token
+    this.tokenExpire = new Date(data.expires_in * 1000)
+  }
+
+  async fetch (endpoint) {
+    // Check if the token is expired
+    if ((this.tokenExpire && new Date() > this.tokenExpire) || !this.tokenExpire) {
+      console.log('osu! API v2 access_token is expired or need to be created')
+      await this.getToken()
+    }
+
+    const { data } = await axios.get(`${this.base}${endpoint}`, {
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+
+
+    return data
+  }
+}
+
 module.exports = {
-  osu: osuApi
+  osu: osuApi,
+  OsuApiv2
 }
