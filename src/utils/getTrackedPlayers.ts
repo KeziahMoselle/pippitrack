760c1f0f0a04e6400f0b5a879dbf13d7700825ec
Nemesis <keziah.moselle@gmail.com>
2021-07-20 18:38:32 +0200

    feat(perf): memoize the getTrackChannels function

--- src/utils/getTrackedPlayers.ts
+++ src/utils/getTrackedPlayers.ts
@@ -1,5 +1,9 @@
+/* eslint-disable camelcase */
+import mem from 'p-memoize'
+import { Client } from 'discord.js'
 import supabase from '../libs/supabase'
-import getTrackChannels from './getTrackChannels'
+import getTrackChannelsFunc from './getTrackChannels'
+const getTrackChannels = mem(getTrackChannelsFunc)
 
 /**
  * Get all tracked players
@@ -7,7 +11,7 @@ import getTrackChannels from './getTrackChannels'
  * @param {*} client Discord.js client
  * @return {*}
  */
-export default async function getTrackedPlayers (client) {
+export default async function getTrackedPlayers (client: Client) {
   // @TODO Paginate them if there is too much to fetch
   const { data: trackedPlayers } = await supabase
     .from('tracked_users')
@@ -18,6 +22,7 @@ export default async function getTrackedPlayers (client) {
   // It allows us to do only one request for the update, then send the embed to multiple channels if needed
   const uniqueTrackedPlayers = {}
 
+  console.time('getTrackedPlayers')
   for (const player of trackedPlayers) {
     // Add the player to the unique list
     if (!uniqueTrackedPlayers[player.osu_id]) {
@@ -28,16 +33,23 @@ export default async function getTrackedPlayers (client) {
       }
 
       // Create the channels array, so we can add multiple guilds to one player
-      const { trackChannel, replayChannel } = await getTrackChannels(player.guild_id, client)
+      const { trackChannel, replayChannel } = await getTrackChannels(
+        player.guild_id,
+        client
+      )
       uniqueTrackedPlayers[player.osu_id].trackChannels = [trackChannel]
       uniqueTrackedPlayers[player.osu_id].replayChannels = [replayChannel]
     } else {
       // We found a duplicate of the player, add the other guild to the array
-      const { trackChannel, replayChannel } = await getTrackChannels(player.guild_id, client)
+      const { trackChannel, replayChannel } = await getTrackChannels(
+        player.guild_id,
+        client
+      )
       uniqueTrackedPlayers[player.osu_id].trackChannels.push(trackChannel)
       uniqueTrackedPlayers[player.osu_id].replayChannels.push(replayChannel)
     }
   }
+  console.timeEnd('getTrackedPlayers')
 
   const count = Object.keys(uniqueTrackedPlayers).length
 
