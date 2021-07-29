5e4d01e16fd8b6ced6c7f88561e3f7b0634232b4
Nemesis <keziah.moselle@gmail.com>
2021-07-29 15:54:56 +0200

    chore: add a try catch to top plays tracking

--- src/services/top/index.ts
+++ src/services/top/index.ts
@@ -26,48 +26,54 @@ export default function update (client: Client): CronJob {
     )
 
     for (const player of trackedPlayers.uniqueTrackedPlayers) {
-      // Get the new top plays from a player
-      const newPlays = await getNewTopPlays(player)
+      try {
+        // Get the new top plays from a player
+        const newPlays = await getNewTopPlays(player)
 
-      if (newPlays.length === 0) {
-        continue
-      }
-
-      // If there is new plays send them to the channel
-      for (const play of newPlays) {
-        const { max_combo } = await osuApiV2.getBeatmap({ id: play.beatmap.id })
-
-        let modsRow = '**NM**'
-        if (play.mods.length > 0) {
-          modsRow = `**${play.mods.length > 0 ? '+' : ''}${play.mods.join(
-            ''
-          )}**`
+        if (newPlays.length === 0) {
+          continue
         }
 
-        const embed = new MessageEmbed()
-          .setAuthor(
-            `New #${play.personalBestIndex} for ${player.osu_username} in ${play.mode}!`,
-            play.user.avatar_url
-          )
-          .setThumbnail(play.beatmapset.covers.list)
-          .setDescription(
-            `**[${play.beatmapset.title}](${play.beatmap.url})** \`[${play.beatmap.version}]\`\n` +
-              `${getEmoji(play.rank)} (${play.beatmap.difficulty_rating} ★)` +
-              ` ${modsRow}\n` +
-              `ᐅ x${play.max_combo}/${max_combo} ᐅ [${play.statistics.count_300}/${play.statistics.count_100}/${play.statistics.count_50}/${play.statistics.count_miss}]`
-          )
-          .addField('PP', `${Math.round(play.pp)}pp`, true)
-          .addField('Accuracy', `${(play.accuracy * 100).toFixed(2)}%`, true)
-          .setFooter('Score set')
-          .setTimestamp(new Date(play.created_at))
+        // If there is new plays send them to the channel
+        for (const play of newPlays) {
+          const { max_combo } = await osuApiV2.getBeatmap({
+            id: play.beatmap.id
+          })
 
-        // Send the embed for each tracked channel linked to this player
-        for (const channel of player.trackChannels) {
-          console.log(
-            `Sending new top play from ${player.osu_username} to #${channel.name}`
-          )
-          channel.send(embed)
+          let modsRow = '**NM**'
+          if (play.mods.length > 0) {
+            modsRow = `**${play.mods.length > 0 ? '+' : ''}${play.mods.join(
+              ''
+            )}**`
+          }
+
+          const embed = new MessageEmbed()
+            .setAuthor(
+              `New #${play.personalBestIndex} for ${player.osu_username} in ${play.mode}!`,
+              play.user.avatar_url
+            )
+            .setThumbnail(play.beatmapset.covers.list)
+            .setDescription(
+              `**[${play.beatmapset.title}](${play.beatmap.url})** \`[${play.beatmap.version}]\`\n` +
+                `${getEmoji(play.rank)} (${play.beatmap.difficulty_rating} ★)` +
+                ` ${modsRow}\n` +
+                `ᐅ x${play.max_combo}/${max_combo} ᐅ [${play.statistics.count_300}/${play.statistics.count_100}/${play.statistics.count_50}/${play.statistics.count_miss}]`
+            )
+            .addField('PP', `${Math.round(play.pp)}pp`, true)
+            .addField('Accuracy', `${(play.accuracy * 100).toFixed(2)}%`, true)
+            .setFooter('Score set')
+            .setTimestamp(new Date(play.created_at))
+
+          // Send the embed for each tracked channel linked to this player
+          for (const channel of player.trackChannels) {
+            console.log(
+              `Sending new top play from ${player.osu_username} to #${channel.name}`
+            )
+            channel.send(embed)
+          }
         }
+      } catch (error) {
+        console.error('diffTopPlays error :', error)
       }
     }
 
