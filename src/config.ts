7130093fdb65f01a98ca5ffa9ad0a1747d93c5ec
Keziah <contact@keziahmoselle.fr>
2024-10-18 19:29:50 +0200

    feat: raise max tracker users in guild to 200

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const apiPort = process.env.PORT || 3000
-export const maxTrackedUsersInGuild = 100
+export const maxTrackedUsersInGuild = 200
 export const maxUsersUpdatedSimultaneously = 15
 export const maxRequestsBeforeSleep = 100
